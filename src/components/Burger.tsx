c5eae91d6142ed6852df86c19ed3741967a04fd2
Károly Török <torok.karoly.krisztian@gmail.com>
2021-04-15 19:07:33 +0200

    Burger color fix

--- src/components/Burger.tsx
+++ src/components/Burger.tsx
@@ -18,13 +18,13 @@ export default function Burger({ active, onClick }: Props) {
             top: 1rem;
             left: 1.25rem;
             z-index: 2;
-            background-color: hsl(var(--primary-hs) var(--l-100));
+            background-color: hsl(var(--background-color));
           }
           .meat {
             position: absolute;
             width: 28px;
             height: 2px;
-            background: hsl(var(--background-color));
+            background: hsl(var(--primary-hs) var(--l-0));
             top: calc(50% - 2px / 2);
             left: calc(50% - 28px / 2);
             transition: all 150ms ease-in;
