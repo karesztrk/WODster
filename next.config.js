52edbe6419fc4ec25bda5415efe4505a13ca895a
Károly Török <torok.karoly.krisztian@gmail.com>
2021-04-15 19:17:37 +0200

    PWA support

--- next.config.js
+++ next.config.js
@@ -1,18 +1,27 @@
-module.exports = ({
-  pageExtensions: ["tsx"],
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pageExtensions: ['tsx'],
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    scope: '/',
+    dest: 'public',
+    sw: 'service-worker.js',
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push(
       ...[
         {
           test: /\.yml$/,
-          type: "json",
-          use: "yaml-loader",
+          type: 'json',
+          use: 'yaml-loader',
         },
         {
           test: /\.svg$/,
-          use: "@svgr/webpack",
+          use: '@svgr/webpack',
         },
-      ]
+      ],
     );
     return config;
   },
